c3a31c1909837e15c7d1d3ddffc9314f4fc73edc
35edwin35 <63353931+35edwin35@users.noreply.github.com>
2020-04-14 16:37:11 +0200

    Update node.js

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 const corlorsToIndex = array => {
- let res = "bleu";
+ let res = "";
  for (let i = 0; i < array.length; i++) {
  res += `Il y a la couleur ${array[i]} à l'index ${i}!`;
  if (i !== array.length - 1) {//si notre index est différent
